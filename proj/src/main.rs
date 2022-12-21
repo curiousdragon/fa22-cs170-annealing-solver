72298f2b19f2d77d26dd87fd57f8460949b856a3
Jamie Hong <jamiehong@berkeley.edu>
2022-12-21 15:26:32 -0800

    [main] refactor and adjust user prompt

--- proj/src/main.rs
+++ proj/src/main.rs
@@ -4,70 +4,91 @@ mod parser;
 mod scorer;
 mod solver;
 use itertools::Itertools;
+use rand::thread_rng;
 use rayon::prelude::*;
 use std::{fs, io};
 
-use rand::thread_rng;
-
-fn run_solver_on_input(iterations: usize, input_name: &str) {
-    println!("Running simulated annealing solver...\n");
+fn run_solver_on_input(iterations: usize, filename: &str) -> String {
     let mut rng = thread_rng();
-    let filepath = format!("inputs/{input_name}");
+    let filepath = format!("inputs/{filename}");
     let g = parser::run(&filepath);
     let (p, cost) = solver::run(&g, iterations, &mut rng);
-    parser::write_output(&format!("outputs/{input_name}-output"), &g, &p);
-    println!(
-        "{filepath}: {0}: {1} + {2} + {3}",
-        cost.loss, cost.weight_loss, cost.num_partition_loss, cost.partition_size_loss
+    parser::write_output(&format!("outputs/{filename}-output"), &g, &p);
+    let score_info = format!(
+        "{filepath}: [{4}] {0} = {1} + {2} + {3}\n",
+        cost.loss,
+        cost.weight_loss,
+        cost.num_partition_loss,
+        cost.partition_size_loss,
+        datatypes::uniq_partitions(&p)
     );
-    println!();
-    println!("Running simulated annealing solver complete!");
+    fs::write(&format!("scores/{filename}-score"), &score_info)
+        .expect(&format!("Could not write to file: scores/{filename}-score"));
+    score_info
+}
+
+fn run_solver_on_given_input(iterations: usize, filename: &str) {
+    eprintln!("Running simulated annealing solver...");
+    let score_info = run_solver_on_input(iterations, filename);
+    eprintln!("\n{}", score_info);
+    eprintln!("Running simulated annealing solver complete!");
 }
 
 fn run_solver_on_all_inputs(iterations: usize) {
-    println!("Running simulated annealing solver...\n");
+    eprintln!("Running simulated annealing solver...");
     let paths = fs::read_dir("inputs").expect("No inputs folder");
     let filepaths = paths.collect_vec();
+    eprintln!("{}", "\n".to_string().repeat(filepaths.len() - 1));
     filepaths.into_par_iter().for_each(|path| {
-        let mut rng = thread_rng();
-        let expected_file = path.expect("No input file");
-        let os_filename = expected_file.file_name();
-        let os_path = expected_file.path();
-        let filename = os_filename.to_str().expect("Invalid input filename");
-        let filepath = os_path.to_str().expect("Invalid input filepath");
-        let g = parser::run(filepath);
-        let (p, cost) = solver::run(&g, iterations, &mut rng);
-        parser::write_output(&format!("outputs/{filename}-output"), &g, &p);
-        println!(
-            "{filepath}: {0}: {1} + {2} + {3}",
-            cost.loss, cost.weight_loss, cost.num_partition_loss, cost.partition_size_loss
-        );
-        let _ = fs::write(
-            &format!("scores/{filename}-score"),
-            &format!(
-                "{filepath}, {4}: {0}: {1} + {2} + {3}\n",
-                cost.loss,
-                cost.weight_loss,
-                cost.num_partition_loss,
-                cost.partition_size_loss,
-                p.partitions.iter().max().unwrap() + 1
-            ),
-        );
+        let os_filename = path.expect("No input file").file_name();
+        let filename = os_filename.to_str().expect("Invalid file name");
+        run_solver_on_input(iterations, filename);
     });
-    println!("Running simulated annealing solver complete!");
+    eprintln!();
+    let paths = fs::read_dir("scores").expect("No scores folder");
+    for path in paths {
+        let score_info =
+            fs::read_to_string(path.expect("No score file").path()).unwrap_or_default();
+        eprintln!("{}", score_info.trim());
+    }
+    eprintln!("\nRunning simulated annealing solver complete!");
 }
 
 fn main() {
-    let iterations = 500000; // 200000; // 10000;
+    let mut iterations = 100000; // 500000; // 200000; // 10000;
 
-    println!("Choose operating mode:");
-    println!("[G] Generate, [A] Solve all ({iterations}), [<input>] Solve <input> ({iterations})");
+    loop {
+        eprintln!("\nChoose operating mode:");
+        eprintln!(
+            concat!(
+                "[G] Generate\n",
+                "[A] Solve all ({iterations}it)\n",
+                "[I] Solve a specific input ({iterations}it)\n",
+                "[N] Set number of iterations"
+            ),
+            iterations = iterations
+        );
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap_or_default();
-    match input.trim() {
-        "G" => generator::run(),
-        "A" => run_solver_on_all_inputs(iterations),
-        input_name => run_solver_on_input(iterations, input_name),
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap_or_default();
+        match input.trim() {
+            "G" => generator::run(),
+            "A" => run_solver_on_all_inputs(iterations),
+            "I" => {
+                eprintln!("[filename] Solve inputs/filename ({iterations}it)");
+                let mut filename = String::new();
+                io::stdin().read_line(&mut filename).unwrap_or_default();
+                run_solver_on_given_input(iterations, &filename.trim());
+            }
+            "N" => {
+                eprintln!("[n] Change the number of iterations to n");
+                let mut n_input = String::new();
+                io::stdin().read_line(&mut n_input).unwrap_or_default();
+                iterations = n_input.trim().parse().expect("Input n is not a usize");
+                continue;
+            }
+            _ => break,
+        }
+        break;
     }
 }
